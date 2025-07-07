d0192757cb332dd1a5e59c05c0eefe7134475c17
kishorekuraganti <krishnakishore9347@gmail.com>
2025-07-07 10:15:33 +0530

    Test Case 1 done

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePage
+{
 	public HomePage(WebDriver driver)
 	{
 		super(driver);
 	}
+	
+	Actions act = new Actions(driver);
 	
 	@FindBy(id="moe-dontallow_button") 
 	WebElement popup;
@@ -18,13 +21,13 @@ public class HomePage extends BasePage{
 	@FindBy(xpath="//input[@id='js-site-search-input' and @type='search']")
 	WebElement searchBar;
 	
-	Actions act = new Actions(driver);
-	
-	public void closePopUp() {
+	public void closePopUp() 
+	{
 		popup.click();	
 	}
 	
-	public void searchBookShelves() {
+	public void searchBookShelves() 
+	{
 		searchBar.sendKeys("book shelves");
 		act.keyDown(Keys.ENTER).keyUp(Keys.ENTER).perform();
 	}
--- src/test/java/testCase/TC_001_DisplayNameAndPrice.java
+++ src/test/java/testCase/TC_001_DisplayNameAndPrice.java
@@ -1,17 +1,26 @@
 package testCase;
 
-import org.testng.annotations.Test;
+import java.io.IOException;
 
+import org.testng.annotations.Test;
 import pageObjects.HomePage;
 import testBase.BaseClass;
+import pageObjects.BookShelvesPage;
 
 public class TC_001_DisplayNameAndPrice extends BaseClass{
 	
 	@Test
-	public void metho() {
+	public void method() throws InterruptedException, IOException 
+	{
 		HomePage hp = new HomePage(driver);
+		BookShelvesPage bsp=new BookShelvesPage(driver);
 		hp.closePopUp();
 		hp.searchBookShelves();
 		System.out.println(driver.getCurrentUrl());
+		bsp.setMinimum("0");
+		bsp.setMaximum("15000");
+		bsp.selectType();
+		bsp.selectOpen();
+		bsp.printShelves();
 	}
 }
