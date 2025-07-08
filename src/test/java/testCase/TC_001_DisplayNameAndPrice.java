aaaa6c5e9e9a1cda5c491b46fbc559a483bb6b6d
suhasKVK <2408701@cognizant.com>
2025-07-08 16:54:44 +0530

    logs modified

--- src/test/java/testCase/TC_001_DisplayNameAndPrice.java
+++ src/test/java/testCase/TC_001_DisplayNameAndPrice.java
@@ -33,7 +33,7 @@ public class TC_001_DisplayNameAndPrice extends BaseClass{
 			logger.info("----- Printing top 3 BookShelves -----");
 			bsp.printShelves();
 
-			logger.error("-----TC_001_DisplayNameAndPrice Passed-----");
+			logger.info("-----TC_001_DisplayNameAndPrice Passed-----");
 			Assert.assertTrue(true);
 
 		} catch (Exception e) {
