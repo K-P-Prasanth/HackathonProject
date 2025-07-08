3c557dd109459524d30667f0d8302e7e786e6e1f
Tamarapalli <2408634@cognizant.com>
2025-07-08 03:27:19 -0700

    logs updated

--- src/test/java/testCase/TC_002_RetriveSubMenuItems.java
+++ src/test/java/testCase/TC_002_RetriveSubMenuItems.java
@@ -15,22 +15,23 @@ public class TC_002_RetriveSubMenuItems extends BaseClass
 	public void method()
 	{
 		try {
-		HomePage hp=new HomePage(driver);
-		HomeAccessoriesPage hap=new HomeAccessoriesPage(driver);
-		logger.info("----- Closing PopUp -----");
-		hp.closePopUp();
-		logger.info("----- Clicking Home Accessories -----");
-		hp.Homeaccesories();
-		logger.info("----- Getting Items from Home Accessories Section -----");
-		hap.getHomeAccessoriesItems();
-		logger.info("----- Listing Items From Home Accessories Section -----");
-		List<String> items = hap.getHomeAccessoriesItems();
-        System.out.println("Home Accessories Sub-Menu Items:");
-        for (String name : items) 
-        System.out.println(name);
-        
-        logger.info("-----TC_002_RetriveSubMenuItems Passed-----");
-        Assert.assertTrue(true);
+			logger.info("-----Starting TC_002_RetriveSubMenuItems-----");
+			HomePage hp = new HomePage(driver);
+			HomeAccessoriesPage hap = new HomeAccessoriesPage(driver);
+			logger.info("----- Closing PopUp -----");
+			hp.closePopUp();
+			logger.info("----- Clicking Home Accessories -----");
+			hp.Homeaccesories();
+			logger.info("----- Getting Items from Home Accessories Section -----");
+			hap.getHomeAccessoriesItems();
+			logger.info("----- Listing Items From Home Accessories Section -----");
+			List<String> items = hap.getHomeAccessoriesItems();
+			System.out.println("Home Accessories Sub-Menu Items:");
+			for (String name : items)
+				System.out.println(name);
+
+			logger.info("-----TC_002_RetriveSubMenuItems Passed-----");
+			Assert.assertTrue(true);
         
 		}catch(Exception e) {
 			logger.error("-----TC_002_RetriveSubMenuItems Failed-----");
